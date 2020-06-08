74e6e926a14272a1addd7e59ff51b9c79b2d47c9
tomas.svoboda <tomas.svoboda@wandera.com>
2020-06-08 16:24:33 +0200

    Task-1: Add json tags

--- client/internal/model/task.go
+++ client/internal/model/task.go
@@ -5,13 +5,13 @@ import (
 	"time"
 )
 
-// TODO TASK-1: Add json tags
-// TODO TASK-2: Add validation tags
+// TODO TASK-1: Add json tags - ID and note are optional
+// TODO TASK-2: Add validation tags - all but note are required
 type Task struct {
-	ID      string
-	Title   string
-	Note    *string
-	DueDate time.Time
+	ID      string    `json:"id,omitempty"`
+	Title   string    `json:"title"`
+	Note    *string   `json:"note,omitempty"`
+	DueDate time.Time `json:"due_date"`
 }
 
 func (t Task) String() string {

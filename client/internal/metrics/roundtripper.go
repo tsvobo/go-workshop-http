1d176b2a381c0bfafbef31364684ab27282af057
tomas.svoboda <tomas.svoboda@wandera.com>
2020-06-08 16:40:28 +0200

    Task-5.2: Implement metrics middleware

--- client/internal/metrics/roundtripper.go
+++ client/internal/metrics/roundtripper.go
@@ -1,15 +1,38 @@
 package metrics
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+const (
+	lblCode     = "code"
+	lblEndpoint = "endpoint"
+)
+
 // TODO TASK-5.2: Implement RoundTripper using promhttp.RoundTripperFunc and record request duration
 func InstrumentRoundTripperDuration(obs prometheus.ObserverVec, next http.RoundTripper) promhttp.RoundTripperFunc {
+	_ = obs.MustCurryWith(prometheus.Labels{
+		lblCode:     "",
+		lblEndpoint: "",
+	})
+
 	return func(r *http.Request) (*http.Response, error) {
-		return next.RoundTrip(r)
+		start := time.Now()
+		resp, err := next.RoundTrip(r)
+		if err == nil {
+			labels := prometheus.Labels{
+				lblCode:     strconv.Itoa(resp.StatusCode),
+				lblEndpoint: fmt.Sprintf("%s %s", r.Method, r.URL.Path),
+			}
+
+			obs.With(labels).Observe(time.Since(start).Seconds())
+		}
+		return resp, err
 	}
 }
